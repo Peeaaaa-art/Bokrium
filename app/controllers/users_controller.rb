9196dd834e7bf3f4772a0e26fc3e0de13a30e160
peeaaaa <bringitback77@gmail.com>
2025-04-17 15:55:56 +0900

    lintチェック

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -15,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'ユーザーを作成しました'
+      redirect_to @user, notice: "ユーザーを作成しました"
     else
       render :new
     end
@@ -28,7 +27,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user, notice: 'ユーザー情報を更新しました'
+      redirect_to @user, notice: "ユーザー情報を更新しました"
     else
       render :edit
     end
@@ -37,7 +36,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_path, notice: 'ユーザーを削除しました'
+    redirect_to users_path, notice: "ユーザーを削除しました"
   end
 
   private
