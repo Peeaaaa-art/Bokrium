8d73f1ed417ce0901466f0d9159ef6c0c20a96b5
peeaaaa <bringitback77@gmail.com>
2025-04-17 20:32:39 +0900

    lint chexk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,12 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
     protected
-  
+
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
     end
   end
-  
 end
