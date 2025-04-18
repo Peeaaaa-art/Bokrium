5a10555c807db7cf4b896d2edab4cefbf84ea912
peeaaaa <bringitback77@gmail.com>
2025-04-18 20:14:33 +0900

    ariaでアクセシビリティ向上

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link_to(name, path, options = {})
+    active = current_page?(path)
+    classes = ["nav-link", "footer-link"]
+    classes << "active fw-semibold" if active
+    options[:class] = classes.join(" ")
+    options[:"aria-current"] = "page" if active
+    link_to name, path, options
+  end
+
 end
