c08df5fabe9c10c0aae16349613300f12ee6db78
peeaaaa <bringitback77@gmail.com>
2025-04-18 17:59:47 +0900

    book:indexの実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
   config.log_level = :debug
   config.logger = Logger.new(STDOUT)
   config.logger.level = Logger::DEBUG
+
+  config.assets.compile = true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "books/index"
+  get "books/show"
+  get "books/new"
+  get "books/create"
+  get "books/edit"
+  get "books/update"
+  get "books/destroy"
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
