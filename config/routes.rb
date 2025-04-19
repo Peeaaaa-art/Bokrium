2f0ef80f9fc72f820675a14e63b8f9166c32177f
peeaaaa <bringitback77@gmail.com>
2025-04-19 11:49:17 +0900

    routingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get "books/index"
-  get "books/show"
-  get "books/new"
-  get "books/create"
-  get "books/edit"
-  get "books/update"
-  get "books/destroy"
   devise_for :users
   resources :users
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "welcome#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
