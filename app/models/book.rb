5218078c96ecdba2c2fde4c454bc5805d15c84a2
peeaaaa <bringitback77@gmail.com>
2025-04-19 12:08:07 +0900

    books, book_tags, tagsの関連付け

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :memos, dependent: :destroy
-  has_many :tags
+  has_many :book_tags, dependent: :destroy
+  has_many :tags, through: :book_tags
 end
