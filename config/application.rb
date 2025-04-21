2b54c5c0cdd86e31511de91d9c0f619e21095031
peeaaaa <bringitback77@gmail.com>
2025-04-21 18:07:12 +0900

    Fix database config for Heroku

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Bokriumm
       Rails.root.join("config/locales/**/*.{yml}")
     ]
     config.time_zone = "Tokyo"
+
+    config.active_record.legacy_connection_handling = false
+    config.active_job.queue_adapter = :async
+    config.action_cable.disable_request_forgery_protection = true
   end
 end
