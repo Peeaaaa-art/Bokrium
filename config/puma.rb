1096cf35e6b1a01aae5e07fba0540be58806fc38
peeaaaa <bringitback77@gmail.com>
2025-04-25 16:49:10 +0900

    lint check

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@ if defined?(Rails) && Rails.env.development? && ENV["USE_SSL"] == "true"
   cert_path = File.expand_path("~/ssl-dev/localhost.crt")
 
   if File.exist?(key_path) && File.exist?(cert_path)
-    ssl_bind '0.0.0.0', '3000', {
+    ssl_bind "0.0.0.0", "3000", {
       key: key_path,
       cert: cert_path
     }
@@ -24,5 +24,3 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-
-
