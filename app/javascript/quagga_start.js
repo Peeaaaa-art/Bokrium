40c526a8d6f6dece7152d9c81d8fa56e31c874b0
peeaaaa <bringitback77@gmail.com>
2025-04-25 12:44:42 +0900

    バーコードスキャンのテスト

---
+++ app/javascript/quagga_start.js
@@ -0,0 +1,37 @@
+import Quagga from "@ericblade/quagga2";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scanner = document.getElementById("scanner");
+
+  if (scanner) {
+    Quagga.init({
+      inputStream: {
+        type: "LiveStream",
+        target: scanner,
+        constraints: {
+          facingMode: "environment" // 背面カメラ指定
+        }
+      },
+      decoder: {
+        readers: ["ean_reader"] // ISBNコード用バーコード
+      }
+    }, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      Quagga.start();
+    });
+
+    Quagga.onDetected((data) => {
+      const isbn = data.codeResult.code;
+      console.log("ISBN Detected:", isbn);
+
+      // ここでinput要素に挿入もできる
+      const input = document.getElementById("isbn_input");
+      if (input) {
+        input.value = isbn;
+      }
+    });
+  }
+});
