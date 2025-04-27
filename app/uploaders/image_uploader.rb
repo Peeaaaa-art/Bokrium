3b80236744418b04f75a0fd11916ae3e4b82bdb6
peeaaaa <bringitback77@gmail.com>
2025-04-27 17:26:29 +0900

    S3のオリジナル削除。これでいいはず

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,35 +32,38 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w[jpg jpeg gif png webp avif tiff]
   end
 
-  after :store_versions, :remove_original
+  after :store, :remove_original
 
   private
-  
+
   def remove_original(_file)
     # モデルが保存済みの場合のみ実行（新規作成時）
     return unless model && model.persisted?
-    
+
     # オリジナルファイルのpath
     original_file = self.file
-    
+
     return unless original_file.present? && original_file.path.present?
-    
+
     # バージョンファイルのpaths
     thumb_exists = versions[:thumb].present? && versions[:thumb].file.present?
     large_exists = versions[:large].present? && versions[:large].file.present?
-    
+
     # バージョンファイルが両方存在することを確認
     if thumb_exists && large_exists
       # オリジナルファイルのファイル名
       original_filename = File.basename(original_file.path)
-      
+
       # バージョンファイル名（thumb_xxx.jpg, large_xxx.jpg）ではないことを確認
       if !original_filename.start_with?('thumb_') && !original_filename.start_with?('large_')
         begin
-          # S3上のオリジナルファイルを削除
-          if original_file.store_dir.include?('uploads')
-            original_file.remove! # S3上での削除処理
+          Rails.logger.info("Deleting original file: #{original_file.path}")
+          
+          # オリジナルファイルを S3 上で削除
+          if original_file.respond_to?(:remove!)
+            original_file.remove!
           else
+            # ローカルストレージの場合
             File.delete(original_file.path) if File.exist?(original_file.path)
           end
         rescue => e
