f478b4a28b3bce47a71217d3972c96bbc4aa2d58
peeaaaa <bringitback77@gmail.com>
2025-04-27 13:18:15 +0900

    uploads_controllerの作成

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,17 @@
+class UploadsController < ApplicationController
+  before_action :authenticate_user!
+
+  def presigned_url
+    s3 = Aws::S3::Resource.new(region: ENV["AWS_DEFAULT_REGION"])
+    bucket = s3.bucket(ENV["S3_BUCKET_NAME"])
+    
+    # 保存場所をここで指定（例：uploads/user_uploads/ランダムファイル名）
+    key = "uploads/#{SecureRandom.uuid}"
+
+    obj = bucket.object(key)
+    
+    url = obj.presigned_url(:put, expires_in: 60, acl: 'public-read')
+
+    render json: { url: url, key: key }
+  end
+end
