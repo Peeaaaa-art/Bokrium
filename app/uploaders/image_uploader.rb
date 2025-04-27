cf023b1d77a7ec053b5b6892e8fe5fb2df256c2d
peeaaaa <bringitback77@gmail.com>
2025-04-27 12:49:12 +0900

    CarrierWaveのコメントを削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,8 +3,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage Rails.env.production? ? :fog : :file
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -29,15 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
   end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_allowlist
     %w[jpg jpeg gif png webp avif tiff]
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg"
-  # end
 end
