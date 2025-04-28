536794900e43c7989e0696ce0f1cb359c76d12b6
peeaaaa <bringitback77@gmail.com>
2025-04-28 20:57:49 +0900

    Fix: アバターが表示されないバグを修正し、大中小バリアント対応をヘルパーに実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,25 @@
 module UsersHelper
-  def user_avatar(user)
-    if user.avatar.present?
-      user.avatar_s3
+  def user_avatar(user, size: :large)
+    image = if user.avatar_s3.attached?
+              variant_for(user.avatar_s3, size)
     else
-      asset_path("avatar_default.jpg")
+              asset_path("avatar_default.jpg")
+    end
+    image
+  end
+
+  private
+
+  def variant_for(avatar, size)
+    case size
+    when :large
+      avatar.variant(resize_to_fill: [ 400, 400 ])
+    when :medium
+      avatar.variant(resize_to_fill: [ 160, 160 ])
+    when :small
+      avatar.variant(resize_to_fill: [ 80, 80 ])
+    else
+      avatar.variant(resize_to_fill: [ 400, 400 ])
     end
   end
 end
