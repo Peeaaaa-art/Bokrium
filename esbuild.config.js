ba6b55aa0694780626716caca2ea7a7c4b28f70a
peeaaaa <bringitback77@gmail.com>
2025-04-30 17:21:23 +0900

    Fix: heroku用にesbuildビルド処理を追加

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,9 @@
-
 const esbuild = require("esbuild");
 
 esbuild.build({
   entryPoints: ["app/javascript/packs/rich_editor.js"],
   bundle: true,
-  outfile: "app/assets/builds/rich_editor.js",
+  outfile: "app/assets/builds/rich_editor.js",  // ← 出力先を変える！
   loader: { ".js": "jsx" },
   sourcemap: true,
   target: ["es6"]
