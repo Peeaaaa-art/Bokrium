dc7f564a81ddfa2aa318ed5390a14cd72f8fd228
peeaaaa <bringitback77@gmail.com>
2025-05-09 23:32:34 +0900

    Style: スマホのbooks#showのビューの改善

--- app/javascript/controllers/tag_collapse_controller.js
+++ app/javascript/controllers/tag_collapse_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const collapses = document.querySelectorAll(".collapse");
+    const collapses = this.element.querySelectorAll(".collapse");
     collapses.forEach(el => el.classList.remove("show"));
   }
 }
--- app/javascript/controllers/tag_modal_controller.js
+++ app/javascript/controllers/tag_modal_controller.js
@@ -1,13 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 import { Modal } from "bootstrap"
 
+// モーダルを安全に切り替える Stimulus controller
 export default class extends Controller {
-  connect() {
-    this.modal = new Modal(this.element)
-    this.modal.show()
+  static values = {
+    targetId: String // 開きたいモーダルの ID を受け取る
   }
 
-  hide() {
-    this.modal.hide()
+  switch() {
+    const currentModalEl = this.element.closest(".modal")
+    const currentInstance = Modal.getInstance(currentModalEl)
+
+    if (currentInstance) {
+      // 現在のモーダルを閉じた後、新しいモーダルを開く
+      currentModalEl.addEventListener(
+        "hidden.bs.modal",
+        () => {
+          const nextModal = document.getElementById(this.targetIdValue)
+          if (nextModal) {
+            const newModal = new Modal(nextModal)
+            newModal.show()
+          }
+        },
+        { once: true }
+      )
+      currentInstance.hide()
+    }
   }
 }
