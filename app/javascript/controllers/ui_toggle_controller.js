c57cd345e76f1e90244aeafdd2ca9bb7f997c2c0
peeaaaa <bringitback77@gmail.com>
2025-05-12 22:49:38 +0900

    Refactor: ソート機能をトグル式のリンクボタンに変更し、デザインを簡素でおしゃれに統一

--- app/javascript/controllers/ui_toggle_controller.js
+++ app/javascript/controllers/ui_toggle_controller.js
@@ -1,16 +1,26 @@
+// controllers/ui_toggle_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["content"]
 
   connect() {
-    // 初期状態：非表示（念のため強制）
-    this.contentTarget.classList.remove("show")
-    this.isVisible = false
+    this.hide()
   }
 
   toggle() {
-    this.isVisible = !this.isVisible
-    this.contentTarget.classList.toggle("show", this.isVisible)
+    this.isVisible ? this.hide() : this.show()
+  }
+
+  show() {
+    this.contentTarget.classList.add("show")
+    this.contentTarget.hidden = false
+    this.isVisible = true
+  }
+
+  hide() {
+    this.contentTarget.classList.remove("show")
+    this.contentTarget.hidden = true
+    this.isVisible = false
   }
 }
