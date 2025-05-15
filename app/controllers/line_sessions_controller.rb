8504a60acd88269cc9b9d1e6d0206287edab0616
peeaaaa <bringitback77@gmail.com>
2025-05-15 13:29:59 +0900

    Add: LINE連携ボタンと説明文をユーザー詳細画面に追加

--- app/controllers/line_sessions_controller.rb
+++ app/controllers/line_sessions_controller.rb
@@ -1,3 +1,5 @@
+# app/controllers/line_sessions_controller.rb
+
 class LineSessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
@@ -5,15 +7,15 @@ class LineSessionsController < ApplicationController
     line_id = auth["uid"]
     line_name = auth["info"]["name"]
 
-    # ログイン中のユーザーに line_id を紐づけ
     if current_user
-      line_user = LineUser.find_or_initialize_by(user_id: current_user.id)
-      line_user.line_id = line_id
-      line_user.line_name = line_name
-      line_user.save!
+      LineUser.find_or_create_by!(user_id: current_user.id) do |line_user|
+        line_user.line_id = line_id
+        line_user.line_name = line_name
+      end
+
       redirect_to user_path(current_user), notice: "LINE連携が完了しました！"
     else
-      redirect_to root_path, alert: "ログインが必要です"
+      redirect_to root_path, alert: "ログインしてからLINE連携してください"
     end
   end
 end
