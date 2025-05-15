58b6b9ab6c73ce506ea4aafa8d2905ada4ca6717
peeaaaa <bringitback77@gmail.com>
2025-05-15 14:05:39 +0900

    Add: LINE連携ユーザーにランダムメモを送信するサービスクラスを実装

---
+++ app/services/line_notification_sender.rb
@@ -0,0 +1,27 @@
+# app/services/line_notification_sender.rb
+class LineNotificationSender
+  def self.send_random_memo_to(user)
+    return unless user&.line_user&.line_id.present?
+    return if user.memos.empty?
+
+    memo = user.memos.order("RANDOM()").first
+    message = Line::Bot::V2::MessagingApi::TextMessage.new(
+      text: "📚 今日のメモ:\n\n#{strip_tags(memo.content).truncate(100)}"
+    )
+
+    request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
+      to: user.line_user.line_id,
+      messages: [ message ]
+    )
+
+    client = Line::Bot::V2::MessagingApi::ApiClient.new(
+      channel_access_token: ENV.fetch("LINE_CHANNEL_TOKEN")
+    )
+
+    client.push_message(push_message_request: request)
+  end
+
+  def self.strip_tags(html)
+    ActionView::Base.full_sanitizer.sanitize(html)
+  end
+end
