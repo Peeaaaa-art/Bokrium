35734ca07d233c3f5a260b9cf6d6b665579d5978
peeaaaa <bringitback77@gmail.com>
2025-05-20 02:06:41 +0900

    Polish: 余白や書籍のサイズを調整

--- app/javascript/controllers/book_shelf_controller.js
+++ app/javascript/controllers/book_shelf_controller.js
@@ -8,11 +8,11 @@ export default class extends Controller {
     if (width < 576) {
       booksPerRow = 5
     } else if (width < 768) {
-      booksPerRow = 6
+      booksPerRow = 7
     } else if (width < 992) {
-      booksPerRow = 8
+      booksPerRow = 9
     } else if (width < 1200) {
-      booksPerRow = 10
+      booksPerRow = 11
     }
 
     const currentParams = new URLSearchParams(window.location.search)
