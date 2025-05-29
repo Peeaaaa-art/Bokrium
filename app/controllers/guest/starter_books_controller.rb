07e30bee2cd295fed6d1a00fce375f7d05a2d949
peeaaaa <bringitback77@gmail.com>
2025-05-29 18:17:12 +0900

    Polish: フッターにヘルプアイコンを追加し、右端に寄せて配置

--- app/controllers/guest/starter_books_controller.rb
+++ app/controllers/guest/starter_books_controller.rb
@@ -1,21 +1,14 @@
 module Guest
   class StarterBooksController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :handle_guest_not_found
-    CHUNKS_PER_PAGE = 14
 
     def index
-      set_know_how_book
-      # display = BooksDisplaySetting.new(session, params, {
-      #   shelf: default_books_per_shelf,
-      #   card: default_card_columns
-      # })
-
-      # @books_per_shelf = display.books_per_shelf
+      set_starter_books
       @books_per_shelf = 5
     end
 
     def show
-      set_know_how_book
+      set_starter_books
       @book = @books.find(params[:id])
       @memos = @book.memos
       @new_memo = Memo.new(book_id: @book.id)
@@ -27,7 +20,7 @@ module Guest
 
     private
 
-    def set_know_how_book
+    def set_starter_books
       isbn_list = %w[9781001001001]
       @books = guest_user.books.where(isbn: isbn_list)
     end
