8a757131d043a9acd0b39a7047d6de48a10cb784
peeaaaa <bringitback77@gmail.com>
2025-05-31 23:37:19 +0900

    Debug: flyioの8080問題

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,22 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-if defined?(Rails) && Rails.env.development? && ENV["USE_SSL"] == "true"
-  key_path  = File.expand_path("~/ssl-dev/localhost.key")
-  cert_path = File.expand_path("~/ssl-dev/localhost.crt")
+# if defined?(Rails) && Rails.env.development? && ENV["USE_SSL"] == "true"
+#   key_path  = File.expand_path("~/ssl-dev/localhost.key")
+#   cert_path = File.expand_path("~/ssl-dev/localhost.crt")
 
-  if File.exist?(key_path) && File.exist?(cert_path)
-    ssl_bind "0.0.0.0", "3000", {
-      key: key_path,
-      cert: cert_path
-    }
-  end
-else
-  port ENV.fetch("PORT", 8080)
-  bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"  # ★ 追加：Fly の警告を抑止するため
-end
+#   if File.exist?(key_path) && File.exist?(cert_path)
+#     ssl_bind "0.0.0.0", "3000", {
+#       key: key_path,
+#       cert: cert_path
+#     }
+#   end
+# else
+#   port ENV.fetch("PORT", 8080)
+#   bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"  # ★ 追加：Fly の警告を抑止するため
+# end
+
+port ENV.fetch("PORT")
 
 plugin :tmp_restart
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
