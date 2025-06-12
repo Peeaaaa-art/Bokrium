aa044ef81d6f326409c805225a8e01b62e13abbc
peeaaaa <bringitback77@gmail.com>
2025-06-12 19:11:30 +0900

    Fix: 検索・ソート・並び替え時にビューを保持して表示するよう修正

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,56 +1,43 @@
 class ExploreController < ApplicationController
   def index
+    @has_books = Book.exists?(user_id: current_user&.id)
     @query = params[:q].to_s.strip
-    @scope = params[:scope]
+    @scope = params[:scope].presence_in(%w[mine public]) || "public"
 
-    uri = URI.parse(request.referer || "")
-    view_mode_from_ref = Rack::Utils.parse_nested_query(uri.query)["view"]
-    @view_mode = view_mode_from_ref.presence_in(%w[shelf card]) || "shelf"
+    display = BooksDisplaySetting.new(session, params, {
+      shelf:       default_books_per_shelf,
+      card:        default_card_columns,
+      detail_card: default_detail_card_columns,
+      spine:       default_spine_per_shelf
+    }, mobile: mobile?)
 
-    @results =
-      if @scope == "mine"
-        search_my_books(@query)
-      else
-        search_public_memos(@query)
+    @view_mode           = display.view_mode
+    @books_per_shelf     = display.books_per_shelf
+    @card_columns        = display.card_columns
+    @detail_card_columns = display.detail_card_columns
+    @spine_per_shelf     = display.spine_per_shelf
+
+    if @scope == "mine"
+      return redirect_to new_user_session_path unless user_signed_in?
+
+      books = current_user.books.includes(:memos, book_cover_s3_attachment: :blob)
+      books = BooksQuery.new(books, params: params, current_user: current_user).call
+
+      if @query.present?
+        title_author_ids = books.search_by_title_and_author(@query).pluck(:id)
+        memo_book_ids = current_user.memos.search_by_content(@query).pluck(:book_id)
+        books = books.where(id: (title_author_ids + memo_book_ids).uniq)
       end
 
-    @books_per_shelf = session[:shelf_per]&.to_i || default_books_per_shelf
-    @card_columns    = session[:card_columns]&.to_i || default_card_columns
-
-    # ✅ Pagyを使ってページネーションする（@resultsはRelation前提）
-    if @scope == "mine"
-      unit_per_page = @view_mode == "shelf" ? @books_per_shelf : @card_columns
-      items_per_page = (unit_per_page * 3).to_i # ← 3段くらい適当に
-      @pagy, @books = pagy(@results, items: items_per_page)
+      books_per_page = (display.unit_per_page * 3).to_i
+      @pagy, @books = pagy(books, items: books_per_page)
     else
+      @results = search_public_memos(@query)
       @pagy, @memos = pagy(@results, items: 20)
     end
 
     if turbo_frame_request?
-      partial_name =
-        if @scope == "mine"
-          @view_mode == "shelf" ? "bookshelf/kino_books_grid" : "bookshelf/simple_card"
-        else
-          "public_bookshelf/public_card"
-        end
-
-      render turbo_stream: turbo_stream.update(
-        "books_frame",
-        partial: partial_name,
-        locals: if @scope == "mine"
-          {
-            books: @books,
-            books_per_shelf: @books_per_shelf,
-            card_columns: @card_columns,
-            pagy: @pagy
-          }
-                else
-          {
-            memos: @memos,
-            pagy: @pagy
-          }
-                end
-      )
+      turbo_render_partial!
     else
       render :index
     end
@@ -58,26 +45,36 @@ class ExploreController < ApplicationController
 
   private
 
-  def search_my_books(query)
-    return Book.none unless user_signed_in?
-
-    books = current_user.books.includes(:memos)
-    return books unless query.present?
-
-    title_author_ids = books.search_by_title_and_author(query).pluck(:id)
-    memo_book_ids = current_user.memos.search_by_content(query).pluck(:book_id)
-
-    current_user.books.where(id: (title_author_ids + memo_book_ids).uniq)
+  def turbo_render_partial!
+    if @scope == "mine"
+      render turbo_stream: turbo_stream.update(
+        "books_frame",
+        partial: "bookshelf/books_frame_wrapper",
+        locals: {
+          view_mode: @view_mode,
+          books: @books,
+          pagy: @pagy,
+          books_per_shelf: @books_per_shelf,
+          card_columns: @card_columns,
+          detail_card_columns: @detail_card_columns,
+          spine_per_shelf: @spine_per_shelf
+        }
+      )
+    else
+      render turbo_stream: turbo_stream.update(
+        "books_frame",
+        partial: "public_bookshelf/public_card",
+        locals: { memos: @memos, pagy: @pagy }
+      )
+    end
   end
 
   def search_public_memos(query)
     scope = Memo.where(visibility: Memo::VISIBILITY[:public_site]).includes(:book, :user)
-
     return scope unless query.present?
 
     book_ids = Book.search_by_title_and_author(query).pluck(:id)
     memo_ids = scope.search_by_content(query).pluck(:id)
-
     scope.where(id: memo_ids).or(scope.where(book_id: book_ids))
   end
 end
