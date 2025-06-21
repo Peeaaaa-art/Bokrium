0f84fb671fdca838093a3a6225772a24d97ed612
peeaaaa <bringitback77@gmail.com>
2025-06-21 16:30:57 +0900

    git commit -m "Remove: 不要なtestディレクトリを削除"

--- test/controllers/pwa_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PwaControllerTest < ActionDispatch::IntegrationTest
-  # test "should get manifest" do
-  #   get pwa_manifest_url
-  #   assert_response :success
-  # end
-end
--- test/controllers/shared_memos_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SharedMemosControllerTest < ActionDispatch::IntegrationTest
-  # test "should get show" do
-  #   get shared_memos_show_url
-  #   assert_response :success
-  # end
-end
