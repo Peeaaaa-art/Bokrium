288c556b0429ed72224928fb09761045ea71e33b
peeaaaa <bringitback77@gmail.com>
2025-07-21 00:58:25 +0900

    Fix: config/vite.rb に `require "vite_ruby"` を追加して ViteRuby の未定義エラーを解消

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require "rails/all"
 
 require "view_component/engine"
 
+require_relative "vite"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
