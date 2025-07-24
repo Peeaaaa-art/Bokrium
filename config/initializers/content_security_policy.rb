49c3b0acb02748aa0d9bf00776de222794ecba0e
peeaaaa <bringitback77@gmail.com>
2025-07-24 23:23:40 +0900

    Chore: 開発・本番環境に対応したCSPポリシーを定義

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,34 +1,22 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
+    policy.font_src    :self, :https, :data, "https://fonts.googleapis.com", "https://fonts.gstatic.com"
+    policy.img_src     :self, :https, :data,
+        "https://assets.bokrium.com", "https://lib.bokrium.com", "https://cdn.bokrium.com", "https://img.bokrium.com", "https://img.hanmoto.com", "https://bokrium-bucket.s3.ap-northeast-1.amazonaws.com",
+        "https://books.google.com", "https://thumbnail.image.rakuten.co.jp", "https://webservice.rakuten.co.jp", "https://image.rakuten.co.jp", "https://rimg.jp"
+    policy.object_src  :none
+    policy.script_src  :self, :https,  "https://assets.bokrium.com"
+    policy.style_src   :self, :https,  "https://assets.bokrium.com", "https://fonts.googleapis.com"
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-# Allow @vite/client to hot reload javascript changes in development
-#    policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+    if Rails.env.development?
+      policy.script_src *policy.script_src, :unsafe_eval, "http://localhost:3036"
+      policy.style_src *policy.style_src, :unsafe_inline
+    end
 
-# You may need to enable this in production as well depending on your setup.
-#    policy.script_src *policy.script_src, :blob if Rails.env.test?
+    policy.connect_src :self, "ws://localhost:3036"
+  end
 
-#     policy.style_src   :self, :https
-# Allow @vite/client to hot reload style changes in development
-#    policy.style_src *policy.style_src, :unsafe_inline if Rails.env.development?
-
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+  config.content_security_policy_report_only = true
+end
