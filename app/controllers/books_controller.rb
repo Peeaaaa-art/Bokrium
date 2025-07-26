755e7c544df7b301f0e87ae94d762adf9d7b6e33
peeaaaa <bringitback77@gmail.com>
2025-07-26 22:00:29 +0900

    Feat: DetailCardビューにTurboによる自動遅延読み込みを実装
    - detail_cardレイアウトでもTurbo Frame + pagyを用いて無限スクロールを実現
    - Books::DetailCardComponent内でTurbo Frameを再帰的に生成
    - controller側ではview_modeに応じたTurbo Streamレスポンスを出力

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -111,6 +111,8 @@ class BooksController < ApplicationController
             locals: { books: @books, spine_per_shelf: @spine_per_shelf, pagy: @pagy, turbo_frame_id: turbo_frame_id }
     when "card"
       render partial: "bookshelf/card_chunk", locals: { books: @books, pagy: @pagy, card_columns: @card_columns }
+    when "detail_card"
+      render partial: "bookshelf/detail_card_chunk", locals: { books: @books, pagy: @pagy, detail_card_columns: @detail_card_columns }
     else
       render :index
     end
