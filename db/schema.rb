125820503eb2ddb48165d53b5a7610be34289610
peeaaaa <bringitback77@gmail.com>
2025-12-30 12:44:29 +0900

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_29_091224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
+  enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
