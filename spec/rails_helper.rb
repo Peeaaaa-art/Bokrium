8d340ff1ddc4754552ad4050f820a6ba5153739a
peeaaaa <bringitback77@gmail.com>
2026-04-19 22:55:50 +0900

    fix: align Playwright version for system specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 # (Docker: docker compose run --rm web-test bundle exec rspec)
 require 'spec_helper'
+ENV["AWS_EC2_METADATA_DISABLED"] ||= "true"
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
