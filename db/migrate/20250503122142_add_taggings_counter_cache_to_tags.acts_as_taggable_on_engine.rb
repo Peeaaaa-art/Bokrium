92b7e1b837b9b12fadc71c432d8dccde7c964b5b
peeaaaa <bringitback77@gmail.com>
2026-05-02 17:03:06 +0900

    fix: remove acts-as-taggable-on migration dependency

--- db/migrate/20250503122142_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++ db/migrate/20250503122142_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
@@ -3,15 +3,21 @@
 # This migration comes from acts_as_taggable_on_engine (originally 3)
 class AddTaggingsCounterCacheToTags < ActiveRecord::Migration[6.0]
   def self.up
-    add_column ActsAsTaggableOn.tags_table, :taggings_count, :integer, default: 0
+    add_column :tags, :taggings_count, :integer, default: 0
 
-    ActsAsTaggableOn::Tag.reset_column_information
-    ActsAsTaggableOn::Tag.find_each do |tag|
-      ActsAsTaggableOn::Tag.reset_counters(tag.id, ActsAsTaggableOn.taggings_table)
-    end
+    execute <<~SQL.squish
+      UPDATE tags
+      SET taggings_count = taggings_counts.count
+      FROM (
+        SELECT tag_id, COUNT(*) AS count
+        FROM taggings
+        GROUP BY tag_id
+      ) AS taggings_counts
+      WHERE tags.id = taggings_counts.tag_id
+    SQL
   end
 
   def self.down
-    remove_column ActsAsTaggableOn.tags_table, :taggings_count
+    remove_column :tags, :taggings_count
   end
 end
--- db/migrate/20250503122144_change_collation_for_tag_names.acts_as_taggable_on_engine.rb
+++ db/migrate/20250503122144_change_collation_for_tag_names.acts_as_taggable_on_engine.rb
@@ -6,8 +6,8 @@
 
 class ChangeCollationForTagNames < ActiveRecord::Migration[6.0]
   def up
-    if ActsAsTaggableOn::Utils.using_mysql?
-      execute("ALTER TABLE #{ActsAsTaggableOn.tags_table} MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
+    if connection.adapter_name.downcase.include?("mysql")
+      execute("ALTER TABLE tags MODIFY name varchar(255) CHARACTER SET utf8 COLLATE utf8_bin;")
     end
   end
 end
